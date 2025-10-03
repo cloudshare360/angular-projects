867ac8ce555f78638e3a4bccd75fbf06e660e845
cloudshare360 <cloudshare360@gmail.com>
2025-10-02 21:09:05 -0500

    updated

--- angular-18-todo-full-stack-app-back-front/Back-End/express-rest-todo-api/app.js
+++ angular-18-todo-full-stack-app-back-front/Back-End/express-rest-todo-api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const compression = require('compression');
+require('dotenv').config();
 
 // Import middleware
 const { 
